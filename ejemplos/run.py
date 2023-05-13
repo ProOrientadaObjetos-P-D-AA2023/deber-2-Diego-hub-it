b0bdc495c807ebfeaf26e453103ac48ec0ebbda4
Diego-hub-it <116037421+Diego-hub-it@users.noreply.github.com>
2023-05-13 15:19:04 -0500

    Update run.py

--- ejemplos/run.py
+++ ejemplos/run.py
@@ -1,3 +1,4 @@
+import mis_clases
 """
 
 """
@@ -6,13 +7,21 @@
 # objeto 01
 
 # crear
-
+asignatura = Asignatura("Matematicas", "MAT-123", 4)
 # Presentar objeto; usar el método __st__
-
+print(asignatura)
 # objeto 02
 
 # crear ingresando valores por teclado
+nombre = input("Ingrese el nombre de la asignatura: ")
+codigo = input("Ingrese el codigo de la asignatura: ")
+creditos = int(input("Ingrese el numero de creditos de la asignatura: "))
+lenguaje = input("Ingrese el lenguaje de programacion de la asignatura: ")
+nombre_docente = input("Ingrese el nombre del docente de la asignatura: ")
+
+mi_asignatura = AsignaturaComputacion(nombre, codigo, creditos, lenguaje, nombre_docente)
 
 # Presentar objeto; usar el método __st__
 
+print(mi_asignatura)
 
