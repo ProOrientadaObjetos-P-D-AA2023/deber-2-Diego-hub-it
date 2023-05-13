477d29bfd4897aed4221d1fda449b66b4f097d74
Diego-hub-it <116037421+Diego-hub-it@users.noreply.github.com>
2023-05-13 15:20:58 -0500

    Update run2.py

--- ejemplos/run2.py
+++ ejemplos/run2.py
@@ -4,15 +4,22 @@
 # Crear dos objetos de la clase 02
 
 # objeto 01
-
+asignatura = Asignatura("Fundamentos de bases de datos", "BD-123", 4)
 # crear
 
 # Presentar objeto; usar el método __st__
+print(asignatura)
 
 # objeto 02
-
 # crear ingresando valores por teclado
+nombre = input("Ingrese el nombre de la asignatura: ")
+codigo = input("Ingrese el codigo de la asignatura: ")
+creditos = int(input("Ingrese el numero de creditos de la asignatura: "))
+lenguaje = input("Ingrese el lenguaje de programacion de la asignatura: ")
+nombre_docente = input("Ingrese el nombre del docente de la asignatura: ")
+
+mi_nueva_asignatura = AsignaturaComputacion(nombre, codigo, creditos, lenguaje, nombre_docente)
 
 # Presentar objeto; usar el método __st__
 
-
+print(mi_nueva_asignatura)
